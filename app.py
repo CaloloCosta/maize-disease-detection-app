1e66c4cb052b002206d37fec204d33aa8a94ec2e
calolocosta <carlosdaviddacosta@gmail.com>
2022-09-09 12:39:12 +0200

    getting stressed

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from app import app
 print(__name__)
 if __name__=='__main__':
-    app.run(debug=True, port=5000, threaded=False)
+    app.run()
+    # app.run(debug=True, host='0.0.0.0', port=5000, threaded=False)
